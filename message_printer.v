b5af9d06761908d037e9691653ae410c37b2542a
t_raviskeller_32@hotmail.com <t_raviskeller_32@hotmail.com>
2016-06-22 17:38:18 -0500

    still a couple of bugs to work out but much closer

--- message_printer.v
+++ message_printer.v
@@ -11,13 +11,18 @@ module message_printer (
   );
   
    //bits is the output to the message_rom module where the data is read to be output into 
-  reg [7:0] bits;
+  reg [7:0] bits [7:0];
   //counter to determine when 8 bits have been input from keyboard
   reg [3:0] ctr_d, ctr_q;
+  reg [71:0] bit_to_rom;
   
   integer check = 0, counter_full = 0,
-          i = 0;
-   
+          i = 0, m = 0;
+  //variables to increment loop to "flatten" 'bits' and put it into bit_to_rom. j = row, k = column          
+  genvar j, k;
+  
+
+       
   localparam STATE_SIZE = 1;
   localparam IDLE = 0,
     PRINT_MESSAGE = 1;
@@ -31,11 +36,12 @@ module message_printer (
   message_rom message_rom (
   .clk(clk),
   .addr(addr_q),
-  .bits_in(bits),
+  .bits_in(bit_to_rom),
   .data(tx_data)
   );
  
   always @(*) begin
+  
     state_d = state_q; // default values
     addr_d = addr_q;   // needed to prevent latches
     new_tx_data = 1'b0;
@@ -57,9 +63,11 @@ module message_printer (
       
     //when check is true (keyboard input is 1 or 0), store keyboard input bit-by-bit in bit_d      
     if (check) 
-      bits[i] = rx_data;
+      bits[i][7:0] = rx_data; 
       i = i + 1;
       ctr_d = ctr_q + 1'b1;
+
+
       
     //counter to determine 8 inputs have been entered
     if (ctr_d == 8)
@@ -72,17 +80,21 @@ module message_printer (
   //if 8 bits have been accepted from the keyboard, reverse the array
   //Really it would be easy to not reverse the array and just output
   //the array in reverse order to gtkterm.
-    bits[0] <= bits[7];
-    bits[1] <= bits[6];
-    bits[2] <= bits[5];
-    bits[3] <= bits[4];
-    bits[4] <= bits[3];
-    bits[5] <= bits[2];
-    bits[6] <= bits[1];
-    bits[7] <= bits[0];
+    bits[0] <= bits[9];
+    bits[1] <= bits[8];
+    bits[2] <= bits[7];
+    bits[3] <= bits[6];
+    bits[4] <= bits[5];
+    bits[5] <= bits[4];
+    bits[6] <= bits[3];
+    bits[7] <= bits[2];
+    bits[8] <= bits[1];
+    bits[9] <= bits[0];
+    
     //now that array is reversed, increment counter_full once more to allow the state to change to PRINT_MESSAGE below
     counter_full <= 2;      
     end
+    
     //after array reversal, change state to PRINT_MESSAGE
         if (counter_full == 2) 
           state_d = PRINT_MESSAGE;
@@ -113,6 +125,17 @@ module message_printer (
     addr_q <= addr_d;
  
       end
+
+//flatten the array and prep it to be sent to message_rom
+ generate
+  
+  for (j = 0; j < 8; j = j + 1) begin
+    for (k = 0; k < 8; k = k + 1) begin
+    assign bit_to_rom[m] = bits[j][k];
+    assign m = m + 1;
+    end
+    end
+endgenerate
  
 endmodule
 
--- message_rom.v
+++ message_rom.v
@@ -1,30 +1,41 @@
 module message_rom (
     input clk,
     //keyboard input from message_printer module
-    input [7:0] bits_in,
+    input [71:0] bits_in,
     input [3:0] addr,
     output [7:0] data
   );
  
-  wire [7:0] rom_data [9:0];
+  wire [7:0] rom_data [7:0];
 
-  integer i = 0;
+//variable for for loop to rebuild the 8 by 9 array for keyboard input addresses
+  genvar j, k;
+//index to bits_to_rom (here the bits_in input)
+  integer m = 0;
 
   reg [7:0] data_d, data_q;
  
   assign data = data_q;
- 
-  always @(*) begin
-    if (addr > 4'd10)
-      data_d = " ";
-    else
-    //assign bits to message_rom
-      for (i = 0; i < 8; i = i + 1) begin
-    assign rom_data[i] = bits_in[i];
+      //assign bits to message_rom
+   
+ generate
+    for (j = 0; j < 8; j = j + 1) begin:first
+    for (k = 0; k < 8; k = k + 1) begin:second
+     assign rom_data[j][k] = bits_in[m];
+     //Verilog doesn't like this m assignment statemetn for some reason
+     assign m = m + 1;
       end
+      end
+      endgenerate
   //assign these at the end of the output    
   assign rom_data[8] = "\n";
   assign rom_data[9] = "\r";
+ 
+  always @(*) begin
+    if (addr > 4'd9)
+      data_d = " ";
+    else
+
  //not my code but theres a counter in message_printer that counts to 8 (i set the value of message length, perhaps it needs
  //to count to 10?
       data_d = rom_data[addr];
