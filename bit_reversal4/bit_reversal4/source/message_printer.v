1ab7f40e315379d52f2e5fed1aaabe5988ac233c
t_raviskeller_32@hotmail.com <t_raviskeller_32@hotmail.com>
2016-07-20 19:32:21 -0500

    it's time to write a test bench...

---
+++ bit_reversal4/bit_reversal4/source/message_printer.v
@@ -0,0 +1,101 @@
+//version 2016-07-15
+module message_printer (
+    input clk,
+    input rst,
+    output [7:0] tx_data,
+    output reg new_tx_data,
+    input tx_busy,
+    input [7:0] rx_data,
+    input new_rx_data,
+    output reg [63:0] bit_to_rom_d, bit_to_rom_q
+  );
+    localparam STATE_SIZE = 1;
+    localparam IDLE = 0,
+    PRINT_MESSAGE = 1;
+    
+ 
+    localparam MESSAGE_LEN = 10; 
+    reg [STATE_SIZE-1:0] state_d, state_q;
+    reg [3:0] addr_d, addr_q;  
+    reg [3:0]counter_q, counter_d;
+     
+  message_rom message_rom (
+  .clk(clk),
+  .addr(addr_q),
+  //connect the keyboard input of this module to the message_rom "bits_in"
+  .bits_in(bit_to_rom_d),
+  .data(tx_data)
+  );
+  
+  //initial begin 
+  //counter_d = 4'd0;
+  //bit_to_rom_d = 63'd0;
+  //end
+ 
+    always @(*) begin
+    state_d = state_q; // default values
+    addr_d = addr_q;   // needed to prevent latches
+    new_tx_data = 1'b0;
+ 
+    case (state_q)
+    //What puts the state into IDLE in the first place?
+      IDLE: begin
+        addr_d = 4'd0;
+         //default values
+       
+    counter_d[3] = counter_q[3];
+    counter_d[2] = counter_q[2];
+    counter_d[1] = counter_q[1];
+    counter_d[0] = counter_q[0];
+    bit_to_rom_d = bit_to_rom_q;   
+       
+    //if a "1" or "0" is typed in the keybaord, assign the input to bit_to_rom
+    if(new_rx_data && rx_data == "0") begin
+    bit_to_rom_d[8 * counter_q + 7-:8] = "0";
+    counter_d = counter_q + 1;
+    end        
+    else if(new_rx_data && rx_data == "1") begin
+    bit_to_rom_d[8 * counter_q + 7-:8] = "1";
+    counter_d = counter_q + 1;
+    end
+    else  begin
+     bit_to_rom_d = bit_to_rom_q;
+      counter_d = counter_q;
+    end
+    if(counter_d == 4'd8) begin
+     state_d = PRINT_MESSAGE;
+     counter_d = 4'd0;
+     end
+     else begin
+     counter_d = counter_q;
+     state_d = IDLE;
+     end
+      end
+      
+      PRINT_MESSAGE: begin
+        if (!tx_busy) begin
+          new_tx_data = 1'b1;
+          addr_d = addr_q + 1'b1;
+          if (addr_q == MESSAGE_LEN-1)
+            state_d = IDLE;
+        end
+      end
+      default: state_d = IDLE;
+    endcase
+  end
+ 
+  always @(posedge clk) begin
+    if (rst) begin
+      state_q <= IDLE;
+      counter_q <= 4'd0;
+      bit_to_rom_q <= 63'd0;
+     end 
+     else begin
+      state_q <= state_d;
+      counter_q <= counter_d;
+      bit_to_rom_q <= bit_to_rom_d;
+      end
+     addr_q <= addr_d; 
+    end
+endmodule
+
---
+++ bit_reversal4/bit_reversal4/source/message_rom.v
@@ -0,0 +1,126 @@
+//version 2016-07-15
+module message_rom (
+    input clk,
+    //keyboard input from message_printer module
+    input [63:0] bits_in,
+    input [3:0] addr,
+    input rst,
+    output [7:0] data
+  );
+//changed this to a 10-by-10 instead of 10-by-8 bc compiler kept flagging that line 41 ("assign rom_data[8]...") was out of range. strangely i didn't get that error for line 42.
+ // wire [7:0] rom_data_q [9:0];
+  reg [7:0] rom_data_d [0:9];
+  reg [7:0] rom_data_q [0:9];
+  wire [7:0] rom_wire [0:9];
+  //reg [7:0] rom_data_q [9:0];
+  reg [3:0] ctr_d, ctr_q;
+  reg [7:0] data_d, data_q;
+  reg [63:0] reverse_bits_d, reverse_bits_q;
+  //reg [3:0] array_d, array_q;
+
+//How can I verify that bits_in has been filled? maybe say if those bits are not null?
+  //initial begin
+   //rom_data_d[8] = "\n";
+   //rom_data_d[9] = "\r";
+  //reverse_bits_d[63:0] <= 64'd0;
+  //end
+  
+  always @(bits_in or ctr_q) begin
+  ctr_d = ctr_q + 1;
+    rom_data_d[9] = "\r";
+    rom_data_d[8] = "\n";
+    rom_data_d[7] = rom_data_q[7];
+    rom_data_d[6] = rom_data_q[6];
+    rom_data_d[5] = rom_data_q[5];
+    rom_data_d[4] = rom_data_q[4];
+    rom_data_d[3] = rom_data_q[3];
+    rom_data_d[2] = rom_data_q[2];
+    rom_data_d[1] = rom_data_q[1];
+    rom_data_d[0] = rom_data_q[0];
+  rom_data_d[ctr_q] = bits_in[63 - 8 * ctr_q -: 8];
+  
+  /*  reverse_bits_d = reverse_bits_q;
+    ctr_d = ctr_q + 1;
+  reverse_bits_d[8 * ctr_q + 7 +: 8] = bits_in[63 - 8 * ctr_q +:8];
+  //assign rom_data
+  //assign rom_data_d[ctr_q] = bits_in[63 - 8 * ctr_q +:8];
+  //assign rom_data[ctr_q] = bits_in[55:48];
+  //assign rom_data[ctr_q] = bits_in[47:40];
+  //assign rom_data[ctr_q] = bits_in[39:32];
+  //assign rom_data[ctr_q] = bits_in[31:24];
+  //assign rom_data[ctr_q] = bits_in[23:16];
+  //assign rom_data[ctr_q] = bits_in[15:8];
+  //assign rom_data[ctr_q] = bits_in[7:0];*/
+  /*if ((reverse_bits[63] == 1'b1) || (reverse_bits[63] == 1'b0)) begin
+  deassign reverse_bits;
+  end*/
+  //if (ctr_d == 4'd8) begin
+    //deassign reverse_bits;
+    //end
+    ctr_d = (ctr_d == 4'd8) ? 0 : ctr_q;
+    //if (ctr_d == 4'd8 ? ctr_d = 0 : ctr_d = ctr_q) 
+     // end
+  /*
+  assign rom_data[0] = reverse_bits_d[7-:8];
+  assign rom_data[1] = reverse_bits_d[15-:8];
+  assign rom_data[2] = reverse_bits_d[23-:8];
+  assign rom_data[3] = reverse_bits_d[31-:8];
+  assign rom_data[4] = reverse_bits_d[39-:8];
+  assign rom_data[5] = reverse_bits_d[47-:8];
+  assign rom_data[6] = reverse_bits_d[55-:8];
+  assign rom_data[7] = reverse_bits_d[63-:8];*/
+ end
+ 
+ assign rom_wire[0] = rom_data_d[0];
+ assign rom_wire[1] = rom_data_d[1];
+ assign rom_wire[2] = rom_data_d[2];
+ assign rom_wire[3] = rom_data_d[3];
+ assign rom_wire[4] = rom_data_d[4];
+ assign rom_wire[5] = rom_data_d[5];
+ assign rom_wire[6] = rom_data_d[6];
+ assign rom_wire[7] = rom_data_d[7];
+ assign rom_wire[8] = rom_data_d[8];
+ assign rom_wire[9] = rom_data_d[9]; 
+  assign data = data_q;
+  
+  always @(*) begin
+    if (addr > 4'd9)
+      data_d = " ";
+    else
+      data_d = rom_wire[addr];
+  end
+ 
+  always @(posedge clk) begin : test
+    if (rst) begin
+    data_q <= 4'd0;
+    ctr_q <= 4'd0;
+   // reverse_bits_q <= 63'd0;
+   rom_data_q[9] <= 8'd0;
+   rom_data_q[8] <= 8'd0;
+   rom_data_q[7] <= 8'd0;
+   rom_data_q[6] <= 8'd0;
+   rom_data_q[5] <= 8'd0;
+   rom_data_q[4] <= 8'd0;
+   rom_data_q[3] <= 8'd0;
+   rom_data_q[2] <= 8'd0;
+   rom_data_q[1] <= 8'd0;
+   rom_data_q[0] <= 8'd0;
+    end
+    else begin
+    rom_data_q[9] <= rom_data_d[9];
+    rom_data_q[8] <= rom_data_d[8];
+    rom_data_q[7] <= rom_data_d[7];
+    rom_data_q[6] <= rom_data_d[6];
+    rom_data_q[5] <= rom_data_d[5];
+    rom_data_q[4] <= rom_data_d[4];
+    rom_data_q[3] <= rom_data_d[3];
+    rom_data_q[2] <= rom_data_d[2];
+    rom_data_q[1] <= rom_data_d[1];
+    rom_data_q[0] <= rom_data_d[0];
+    data_q <= data_d;
+    ctr_q <= ctr_d;
+    //reverse_bits_q <= reverse_bits_d;
+    end
+  end
+ 
+endmodule
