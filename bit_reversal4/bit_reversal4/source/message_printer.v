39d7c1252edeeb86954f3af049b40277f17dacba
Trav <t_raviskeller_32@hotmail.com>
2016-08-09 19:39:14 -0500

    Rewrote and simulated.  Passed but may have timing issues
    I took out the state machine and rewrote the code to pass my test bench.  The first input is giving me problems and is not getting reversed in message_rom.  It just shows XXXXXXXX.  This may have timing issues when I synthesize it and it will definitely have latches.

--- bit_reversal4/bit_reversal4/source/message_printer.v
+++ bit_reversal4/bit_reversal4/source/message_printer.v
@@ -4,118 +4,106 @@ module message_printer (
     output [7:0] tx_data,
     output reg new_tx_data,
     input tx_busy,
-    input [7:0] rx_data,
-    input new_rx_data,
-    input rst
+	 input [7:0] rx_data,
+	 input new_rx_data,
+    input rst,
+	 output state,
+	 output [3:0] counter,
+	 output bytes,
+	 output [3:0] addr
     //need to have one register for output that has its value passed into it by another register
-   
+
   );
     localparam STATE_SIZE = 1;
     localparam IDLE = 0,
     PRINT_MESSAGE = 1;
-    wire [23:0] bit_to_rom;
-    reg [23:0] bit_to_rom_d, bit_to_rom_q;
-    
- 
-    localparam MESSAGE_LEN = 5; 
-    reg [STATE_SIZE-1:0] state_d, state_q;
-    reg [2:0] addr_d, addr_q;  
-    reg [1:0]counter_q, counter_d;
-     
-  message_rom message_rom (
+    //wire [23:0] bit_to_ram;
+    reg byte_out_d, byte_out_q;
+    //wire byte_out;
+
+    //use counter in this module for incrementation in message_ram
+    //assign counter = counter_q;
+    //assign byte_out = byte_out_q;
+    localparam MESSAGE_LEN = 10;
+    reg /*[STATE_SIZE-1:0]*/ state_d, state_q;
+    reg [3:0] addr_d, addr_q;
+    reg [3:0]counter_q, counter_d;
+  assign state = state_q;
+  assign counter = counter_q;
+  assign bytes = byte_out_q;
+  assign addr = addr_q;
+message_ram message_ram1 (
   .clk(clk),
   .addr(addr_q),
-  //connect the keyboard input of this module to the message_rom "bits_in"
-  .bits_in(bit_to_rom),
-  .data(tx_data)
+  //connect the keyboard input of this module to the message_ram "byte_in"
+  .byte_in(byte_out_q),
+  .data(tx_data),
+  .counter(counter),
+  .new_rx_data(new_rx_data)
   );
-  
- 
+
+
     always @(*) begin
+	 if(new_rx_data)
+	 begin
+			if (rx_data == "0")
+			begin
+				byte_out_d = 0;
+				counter_d = counter_q + 1;
+				//new_tx_data = 1;
+			end
+
+		else if(rx_data == "1")
+		begin
+			byte_out_d = 1;
+			counter_d = counter_q + 1;
+		end
+	end
+
+	//default values
+	else
+	begin
+		//counter_d = counter_q;
+		//byte_out_d = byte_out_q;
+		addr_d = addr_q;
+	end
+
+	if (counter_d == 8 && !tx_busy)
+	begin
+       new_tx_data = 1'b1;
+	    addr_d = addr_q + 1'd1;
+
+		  if (addr_d > 10)
+			counter_d = 4'd0;
+			//addr_d = 4'd0;
+	end
+
+	//execution is skipping all my other logic and going straight for this else statement. Why????
+	//defaults
+    /*else begin
     state_d = state_q; // default values
     addr_d = addr_q;   // needed to prevent latches
-    new_tx_data = 1'b0;
-    bit_to_rom_d = bit_to_rom_q;
+    byte_out_d = byte_out_q;
     counter_d = counter_q;
- 
-    case (state_q)
-    //What puts the state into IDLE in the first place?
-      IDLE: begin
-        addr_d = 4'd0;
-        
-       
-       
-    //if a "1" or "0" is typed in the keybaord, assign the input to bit_to_rom
-    if(new_rx_data && rx_data == "0") begin
-     //default
-    //bit_to_rom_d = bit_to_rom_q;
-    //send this byte to rom
-    bit_to_rom_d[8 * counter_q + 7-:8] = "0";
-    counter_d = counter_q + 1'b1;
-    end        
-    
-    else if(new_rx_data && rx_data == "1") begin
-    //default
-    //bit_to_rom_d = bit_to_rom_q;
-    //send this byte to rom
-    bit_to_rom_d[8 * counter_q + 7-:8] = "1";
-    counter_d = counter_q + 1'b1;
     end
-    
-    else begin
-      bit_to_rom_d = bit_to_rom_q;
-      counter_d = counter_q;
-    end
-    
-    if(counter_d == 2'd3) begin
-     state_d = PRINT_MESSAGE;
-     counter_d = 2'd0;
-     end
-     else begin
-     state_d = IDLE;
-     counter_d = counter_q;
-     end
-      end
-      
-      PRINT_MESSAGE: begin
-        if (!tx_busy) begin
-          //bit_to_rom_d = bit_to_rom_q;
-          //counter_d = counter_q;
-          new_tx_data = 1'b1;
-          addr_d = addr_q + 1'b1;
-          if (addr_q == MESSAGE_LEN-1) begin
-            state_d = IDLE;
-            end
-         //else begin
-          //bit_to_rom_d = bit_to_rom_q;
-          //counter_d = counter_q;
-          //end   
-        end
-      end
-      
-      default: begin
-      state_d = IDLE;
-      //bit_to_rom_d = bit_to_rom_q;
-      //counter_d = counter_q;
-      end
-      
-    endcase
-  end
- 
- assign bit_to_rom = bit_to_rom_d;
- 
+	 */end
+
+   
+
+ assign bit_to_ram = byte_out_d;
+
   always @(posedge clk) begin
     if (rst) begin
-      state_q <= IDLE;
-      counter_q <= 2'd0;
-      bit_to_rom_q <= 23'd0;
-     end 
+      state_q <= 1'd0;
+      counter_q <= 4'd0;
+      byte_out_q <= 1'd0;
+		addr_q <= 4'd0;
+     end
      else begin
       state_q <= state_d;
       counter_q <= counter_d;
-      bit_to_rom_q <= bit_to_rom_d;
-      end
-     addr_q <= addr_d; 
+      byte_out_q <= byte_out_d;
+      addr_q <= addr_d;
+	 end
     end
 endmodule
-
--- bit_reversal4/bit_reversal4/source/message_tb.v
+++ bit_reversal4/bit_reversal4/source/message_tb.v
@@ -1,53 +1,126 @@
-module message_tb ();
-   
+module message_tb;
+
+  initial begin
+    clk = 1'b0;
+    rst = 1'b1;
+    repeat(4) #10 clk = ~clk;
+    rst = 1'b0;
+	 tx_busy = 1'd0;
+    new_rx_data = 0;
+	 forever #10 clk = ~clk; // generate a clock
+
+	 $finish;
+  end
+
+   initial begin
+	 //1
+	 new_rx_data = 1;
+	 rx_data = "1";
+
+	 //2
+    #100
+	 new_rx_data = 1;
+    rx_data = "0";
+	 //wait to set the new input flag back to zero after input has been processed.
+	 #1
+	 new_rx_data = 0;
+
+	 //3
+	 #100
+	 new_rx_data = 1;
+    rx_data = "1";
+	 #1
+	 new_rx_data = 0;
+
+	 //4
+    #100
+    new_rx_data = 1;
+    rx_data = "0";
+	 #1
+	 new_rx_data = 0;
+
+	 //5
+	 #100
+	 new_rx_data = 1;
+    rx_data = "1";
+	 #1
+	 new_rx_data = 0;
+
+	 //6
+	 #100
+	 new_rx_data = 1;
+	 rx_data ="0";
+	 #1
+	 new_rx_data = 0;
+
+	 //7
+	 #100
+	 new_rx_data = 1;
+	 rx_data = "1";
+	 #1
+	 new_rx_data = 0;
+
+	 //8
+	 #100
+	 new_rx_data = 1;
+	 rx_data = "0";
+	 #1
+	 new_rx_data = 0;
+
+	 #6000
+    $finish;
+    end
+
+
+
+
+
   reg clk, rst;
   wire [7:0] tx_data;
-  reg new_tx_data;
+  wire new_tx_data;
   reg tx_busy, new_rx_data;
   reg [7:0] rx_data;
-  
-  
+  //reg [0:7] data;
+  wire [3:0] addr;
+  wire[3:0] counter;
+  wire bytes;
+  //wire [7:0] data;
+  wire state;
 
- 
-   
- message_printer DUT (
+
+
+
+
+ message_printer message_printer1 (
     .clk(clk),
     .rst(rst),
     .tx_data(tx_data),
     .new_tx_data(new_tx_data),
     .tx_busy(tx_busy),
     .new_rx_data(new_rx_data),
-    .rx_data(rx_data)
+    .rx_data(rx_data),
+	 //.byte_out_q(bytes)
+	 .state(state),
+	 .addr(addr),
+	 .bytes(bytes),
+	 .counter(counter)
+
+
   );
-   
-   
-  initial begin
-    clk = 1'b0;
-    rst = 1'b1;
-    repeat(4) #10 clk = ~clk;
-    rst = 1'b0;
-    forever #10 clk = ~clk; // generate a clock
-  end
-  
-  initial begin
-		tx_busy = 0;
-	 new_rx_data = 0;
-	 rx_data = "0";
-    @(negedge rst); begin
-    new_rx_data = 1;
-    rx_data = "0";
-    end
-    repeat(300) @(posedge clk); begin
-    new_rx_data = 1;
-    rx_data = "1";
-    end
-    repeat(300) @(posedge clk); begin
-    
-    new_rx_data = 1;
-    rx_data = "1";
-    end
-    $finish;
-    end
-    
-   
+
+
+
+  /*message_rom DUT1 (
+	.clk(clk),
+  .data(data)
+  //.byte_in(bytes),
+  //.addr(addr),
+  //.counter(counter)
+    );*/
+
+
+
+
+
+
 endmodule
